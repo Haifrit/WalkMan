1aa449d6cefae4e653479dd2f07a00ee26cac6d2
Haifrit <janphilipwolter@gmail.com>
2016-11-13 19:04:22 +0100

    Setup
     Changed Projekt struckture to a LibGdX Projekt

---
+++ android/src/wolter/philip/jan/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package wolter.philip.jan.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import wolter.philip.jan.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
