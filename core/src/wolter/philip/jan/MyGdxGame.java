24b80c5cd2e16aef5d2a778cb7f3fd1b6c180d7f
Haifrit <janphilipwolter@gmail.com>
2016-11-16 20:08:02 +0100

    feat
    add Orthogonal Camera

--- core/src/wolter/philip/jan/MyGdxGame.java
+++ core/src/wolter/philip/jan/MyGdxGame.java
@@ -2,7 +2,9 @@ package wolter.philip.jan;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,6 +14,7 @@ import javafx.scene.input.KeyCode;
 public class MyGdxGame extends ApplicationAdapter {
 
 	private static final String LOG_TAG =MyGdxGame.class.getSimpleName();
+	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
 	
@@ -19,6 +22,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 720, 1280);
 	}
 
 	@Override
@@ -27,17 +32,16 @@ public class MyGdxGame extends ApplicationAdapter {
 		float yCord = 0;
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, xCord, yCord);
-		batch.end();
-
 		if (Gdx.input.isTouched()) {
 			xCord = Gdx.input.getX();
 			yCord = Gdx.input.getY();
 			Gdx.app.log(LOG_TAG, " Screen has been touched at X = " + xCord + " Y = " + yCord);
-			batch.begin();
 			batch.draw(img, xCord, yCord);
-			batch.end();
 		}
+		batch.end();
 	}
 }
