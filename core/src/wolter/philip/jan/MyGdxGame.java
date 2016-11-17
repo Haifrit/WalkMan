3a063803480cc2083fb7dc815aeb353f85f953c0
Haifrit <janphilipwolter@gmail.com>
2016-11-17 23:52:59 +0100

    feat
    Added graphics and filled the background

--- core/src/wolter/philip/jan/MyGdxGame.java
+++ core/src/wolter/philip/jan/MyGdxGame.java
@@ -16,14 +16,16 @@ public class MyGdxGame extends ApplicationAdapter {
 	private static final String LOG_TAG =MyGdxGame.class.getSimpleName();
 	OrthographicCamera camera;
 	SpriteBatch batch;
-	Texture img;
+	Texture img,grass,stone,walkie;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		grass = new Texture("grass.png");
+		stone = new Texture("Stone.png");
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 720, 1280);
+		camera.setToOrtho(true, 352, 608);
 	}
 
 	@Override
@@ -35,13 +37,21 @@ public class MyGdxGame extends ApplicationAdapter {
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, xCord, yCord);
+		drawBackground();
 		if (Gdx.input.isTouched()) {
 			xCord = Gdx.input.getX();
 			yCord = Gdx.input.getY();
 			Gdx.app.log(LOG_TAG, " Screen has been touched at X = " + xCord + " Y = " + yCord);
-			batch.draw(img, xCord, yCord);
+			batch.draw(stone, xCord, yCord);
 		}
 		batch.end();
+	}
+
+	private void drawBackground () {
+		for (int x = 0; x <= 352; x = x + 32) {
+			for (int y = 0; y <= 608; y = y + 32) {
+				batch.draw(grass, x, y);
+			}
+		}
 	}
 }
